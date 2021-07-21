682cbab8e093566ad7ccc6246f346cd22bf2328e
Justin Tubay <justin@Justins-MBP.lan>
2021-07-20 22:41:10 -0400

    contact form

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,40 +1,40 @@
-import React, {useState} from 'react';
+//import React, {useState} from 'react';
 import '../../Components/Styles/Styles.css';
 import {Container} from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-import { init, sendForm } from 'emailjs-com';
-const {REACT_APP_EMAIL_FORM_KEY} = process.env;
+//import { useForm } from 'react-hook-form';
+//import { init, sendForm } from 'emailjs-com';
+//const {REACT_APP_EMAIL_FORM_KEY} = process.env;
 
-init(`user_EJB0UvP229Yp1EqevHUIz`);
+//init(`user_EJB0UvP229Yp1EqevHUIz`);
 //user_EJB0UvP229Yp1EqevHUIz
 const Contact = () => {
-    const { register, handleSubmit} = useForm();
-    const [contactNumber, setContactNumber] = useState("000000");
-    const [statusMessage, setStatusMessage] = useState("");
+    // const { register, handleSubmit} = useForm();
+    // const [contactNumber, setContactNumber] = useState("000000");
+    // const [statusMessage, setStatusMessage] = useState("");
 
-    console.log(REACT_APP_EMAIL_FORM_KEY)
-    const generateContactNumber = () => {
-        const numStr = "000000" + (Math.random() * 1000000 | 0);
-        setContactNumber(numStr.substring(numStr.length - 6));
-      }
-      const onSubmit = (data) => {
-        const form = document.querySelector('#con')
-        console.log(data);
-        generateContactNumber();
-        sendForm('default_service', 'template_wmswmt9', '#con')
-          .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            form.reset();
-            setStatusMessage("Message sent!");
-            setTimeout(()=> {
-            }, 5000)
-          }, function(error) {
-            console.log('FAILED...', error);
-            setStatusMessage("Failed to send message! Please try again later.");
-            setTimeout(()=> {
-            }, 5000)
-          });
-      }
+    // console.log(REACT_APP_EMAIL_FORM_KEY)
+    // const generateContactNumber = () => {
+    //     const numStr = "000000" + (Math.random() * 1000000 | 0);
+    //     setContactNumber(numStr.substring(numStr.length - 6));
+    //   }
+    //   const onSubmit = (data) => {
+    //     const form = document.querySelector('#con')
+    //     console.log(data);
+    //     generateContactNumber();
+    //     sendForm('default_service', 'template_wmswmt9', '#con')
+    //       .then(function(response) {
+    //         console.log('SUCCESS!', response.status, response.text);
+    //         form.reset();
+    //         setStatusMessage("Message sent!");
+    //         setTimeout(()=> {
+    //         }, 5000)
+    //       }, function(error) {
+    //         console.log('FAILED...', error);
+    //         setStatusMessage("Failed to send message! Please try again later.");
+    //         setTimeout(()=> {
+    //         }, 5000)
+    //       });
+    //   }
       
         return (
             
@@ -46,19 +46,17 @@ const Contact = () => {
                             
                             <div class="contact-container">
                                 
-                                <form onSubmit={handleSubmit(onSubmit)} id="con">
-                                <h6 className='status-message'>{statusMessage}</h6>
+                                <form action = 'mailto:mpacharlotte@gmail.com' id="con">
                                     <h3>Contact Form</h3>
                                     <fieldset>
-                                    <input type='hidden' name='contact_number' value={contactNumber} />
 
-                                        <input name="user_name" {...register("user_name")} placeholder="Your name" type="text" tabindex="1" required autofocus/>
+                                        <input name="user_name" placeholder=' Your Name' type="text" tabindex="1" required autofocus/>
                                     </fieldset>
                                     <fieldset>
-                                        <input name="user_email" {...register("user_email")} placeholder="Your Email Address" type="email" tabindex="2" required/>
+                                        <input name="user_email"  placeholder="Your Email Address" type="email" tabindex="2" required/>
                                     </fieldset>
                                     <fieldset>
-                                        <textarea name="message" {...register("message")} placeholder="Type your message here...." tabindex="5" type="message" required></textarea>
+                                        <textarea name="message"  placeholder="Type your message here...." tabindex="5" type="message" required></textarea>
                                     </fieldset>
                                     <fieldset>
                                         <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
