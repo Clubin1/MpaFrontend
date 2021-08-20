ceb7529c45ec14400db1b5d78baf893751bdb4d9
Justin Tubay <justin@Justins-MBP.lan>
2021-08-20 17:54:34 -0400

    moved search directory section to home page

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -61,7 +61,49 @@ const Home = () => {
                     </Row>
 
                 </Container>
+                <div className="searchContainer">
+                <p className="search-info twotime">
+                                Welcome to the Mecklenburg
+                                                                                                				Psychological Association (MPA) Psychologist Membership & Referral
+                                                                                                				Directory. Search for disorders, location and name.
+                            </p>
+                            <p className="search-info">
+                                The Mecklenburg Psychological Association provides this
+                                                                                                				service as a way for you to find licensed psychologists in your
+                                                                                                				area. Using our search engine, you will receive a list of
+                                                                                                				psychologists whose self-described services match with your needs.
+                                                                                                				You may click on any psychologist's name to find out more about
+                                                                                                				that psychologist. Many psychologists have web pages that further
+                                                                                                				describe their services.
+                            </p>
+                            <p className="search-info">
+                                You may want to contact at least three psychologists by phone
+                                                                                                				before making a decision about with whom you wish to schedule an
+                                                                                                				initial appointment. You may want to discuss how much experience
+                                                                                                				the psychologist has had with patients/clients with problems
+                                                                                                				similar to the one(s) you have, the cost of services, how
+                                                                                                				frequently you would be seen, the estimated length of treatment
+                                                                                                				and any questions you have about training or experience. If you
+                                                                                                				plan to use medical insurance to help cover the cost of therapy,
+                                                                                                				you may want to call your insurance company to understand your
+                                                                                                				mental health benefits, including co-pays and deductibles for in-
+                                                                                                				or out-of-network providers. Please consider that you may need to
+                                                                                                				leave a phone number and a time you can be reached in case the
+                                                                                                				therapist is unavailable when you call.
+                            </p>
 
+                            <p className="search-info">
+                                Psychologists in the MPA Membership & Referral Directory are
+                                                                                                				licensed in the State of North Carolina. Psychologists provide
+                                                                                                				testing/assessment, psychotherapy, consultation and other
+                                                                                                				services. Psychologists do not prescribe medication. MPA hopes
+                                                                                                				that this information will assist you in selecting a suitable
+                                                                                                				therapist. Psychologists listed in this directory and their
+                                                                                                				identified areas of specialization are based on self-report. MPA
+                                                                                                				assumes no liability in the event you are not satisfied or helped
+                                                                                                				by a psychologist you choose to meet with from this list.
+                            </p>
+                </div>
             </div>
 		</div>
 	)
